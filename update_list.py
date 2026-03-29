81c36f48164c5c1c520d377f536a5bce2cc335d6
sasan-jafarzadeh <128408520+sasan-jafarzadeh@users.noreply.github.com>
2026-03-29 12:27:12 -0600

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -7,10 +7,12 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 # ==============================
 
 PLAYLIST_URLS = [
+"https://iptv-org.github.io/iptv/languages/tur.m3u",
     "https://iptv-org.github.io/iptv/countries/tr.m3u",
     "https://onureroz.com/indirmeler/turk/index.m3u",
-    "https://raw.githubusercontent.com/hodhodfarsi/iptv-for-iran/refs/heads/main/ir2.m3u",
-    "https://iptv-org.github.io/iptv/languages/fas.m3u"
+    "https://iptv-org.github.io/iptv/languages/fas.m3u",      # بهترین برای زبان فارسی
+    "https://iptv-org.github.io/iptv/countries/ir.m3u",       # کشور ایران (IRIB و ...)
+    "https://raw.githubusercontent.com/hodhodfarsi/iptv-for-iran/refs/heads/main/ir2.m3u"
 ]
 
 HEADERS = {
